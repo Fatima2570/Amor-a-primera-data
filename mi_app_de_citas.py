63aee80ae43ee8034de25c55fa421dee1c130846
Fatima2570 <a20230963@pucp.edu.pe>
2025-07-10 19:29:09 -0500

    Update mi_app_de_citas.py

--- mi_app_de_citas.py
+++ mi_app_de_citas.py
@@ -7,7 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1UcvVKqphG_csJ7ktFoZMXD7ONxvX0zAn
 """
 
-pip install streamlit
 
 import streamlit as st
 import pandas as pd
